3a64c4d047ed86d1e3caad5760a1982419558bba
Leejunheezz123 <dotm3434@naver.com>
2022-01-05 05:07:34 +0900

    board 디자인 중

--- routes/admin/board-rotuer.js
+++ routes/admin/board-rotuer.js
@@ -3,9 +3,20 @@ const express = require("express");
 const router = express.Router();
 const { error } = require("../../modules/util");
 
-router.get("/", (req, res, next) => {
-  res.render("admin/board/board-list", { css: "admin-board" });
+router.get("/init", (req, res, next) => {
+  res.render("admin/board/board-init", { css: "admin-board" });
 });
+
+router.get("/", (req, res, next) => {
+  const boardType = req.query.type || "default";
+  res.render("admin/board/board-list", { css: "admin-board", boardType });
+});
+
+router.get("/:id", (req, res, next) => {
+  const boardType = req.query.type || "default";
+  res.render("admin/board/board-view", { css: "admin-board", boardType });
+});
+
 router.post("/", (req, res, next) => {
   res.send("/admin/board:POST");
 });
@@ -14,9 +25,6 @@ router.put("/", (req, res, next) => {
 });
 router.delete("/", (req, res, next) => {
   res.send("/admin/board:DELETE");
-});
-router.get("/init", (req, res, next) => {
-  res.render("admin/board/board-init", { css: "admin-board" });
 });
 router.post("/init", (req, res, next) => {
   res.send("/admin/board/init:POST");
