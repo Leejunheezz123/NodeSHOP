1635d67801d021a4414a54d0d85b93494d8eacf9
Leejunheezz123 <dotm3434@naver.com>
2022-01-14 00:08:24 +0900

    after-multer, board-router 작업

--- middlewares/after-multer-mw.js
+++ middlewares/after-multer-mw.js
@@ -1,11 +1,13 @@
 module.exports = (fields) => {
   return (req, res, next) => {
+    const files = [];
     for (let field of fields) {
       req.files[field].forEach((v) => {
         v.oriName = v.originalname;
         v.saveName = v.filename;
         v.mimeType = v.mimetype;
         v.fileType = field === "img" ? "I" : "F";
+        files.push(v);
       });
     }
     next();
