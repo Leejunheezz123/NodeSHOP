62284a4e315404f9dedfbb57a9f6871ecada1e5b
Leejunheezz123 <dotm3434@naver.com>
2022-01-14 00:39:07 +0900

    BOARD SAVE 완료

--- middlewares/after-multer-mw.js
+++ middlewares/after-multer-mw.js
@@ -2,14 +2,17 @@ module.exports = (fields) => {
   return (req, res, next) => {
     const files = [];
     for (let field of fields) {
-      req.files[field].forEach((v) => {
-        v.oriName = v.originalname;
-        v.saveName = v.filename;
-        v.mimeType = v.mimetype;
-        v.fileType = field === "img" ? "I" : "F";
-        files.push(v);
-      });
+      if (req.files && req.files[field]) {
+        req.files[field].forEach((v) => {
+          v.oriName = v.originalname;
+          v.saveName = v.filename;
+          v.mimeType = v.mimetype;
+          v.fileType = field === "img" ? "I" : "F";
+          files.push(v);
+        });
+      }
     }
+    req.files = files;
     next();
   };
 };
