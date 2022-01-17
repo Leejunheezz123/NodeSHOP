98eb11de6050b85f9d17bf993801f8b58e81f0ee
Leejunheezz123 <dotm3434@naver.com>
2022-01-17 19:12:23 +0800

    passport 작업중

--- passport/index.js
+++ passport/index.js
@@ -1,39 +1,21 @@
-/* 
-1. 로그인 -> serialize : browser의 cookie에 idx 남기기
-2. 재접속 할때마다 -> deserialize -> req.user
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models");
 
-3. strategy 로직
-	가. 로그인 라우터의 미들웨어 passport.authenticate(['local', 'kakao', 'naver'])를 거친다.
-	나. 각 Strategy로 가서 
-	다. local은 done() 실행
-	라. kakao/naver는 passport-kakao(naver)가 done을 내장하고 있으므로 미들웨어로만 넣어준다.
-*/
+const cb = async (userid, userpw, done) => {
+  try {
+    const user = await User.loginUser(userid, userpw);
+    if (user) done(null, user);
+    else done(null, false, "아이디와 패스워드를 확인하세요.");
+  } catch (err) {
+    done(err);
+  }
+};
 
+const fields = {
+  usernameField: "userid",
+  passwordField: "userpw",
+};
 
-const local = require('./local-strategy')
-const kakao = require('./kakao-strategy')
-const naver = require('./naver-strategy')
-const { findUser } = require('../models/auth')
+const localStrategy = new LocalStrategy(fields, cb);
 
-const serialize = (user, done) => {
-	done(null, user.idx)
-}
-
-const deserialize = async (idx, done) => {
-	try {
-		const { success, user } = await findUser('idx', idx)
-		if(success) done(null, user)
-		else done(null, false, '사용자 정보가 없습니다.')
-	}
-	catch(err) {
-		done(err)
-	}
-}
-
-module.exports = passport => {
-	passport.serializeUser(serialize)				// req.user -> idx (cookie -> session)
-	passport.deserializeUser(deserialize)		// req.user <- DB user 정보 (session)
-	local(passport)
-	kakao(passport)
-	naver(passport)
-}
+module.exports = (passport) => passport.use(localStrategy);
