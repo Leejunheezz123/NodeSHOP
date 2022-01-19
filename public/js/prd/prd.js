234a879dcebafaabcf333a0669790a7fff424240
Leejunheezz123 <dotm3434@naver.com>
2022-01-19 09:58:36 +0800

    prd-form.ejs

--- public/js/prd/prd.js
+++ public/js/prd/prd.js
@@ -24,3 +24,14 @@ var quill = new Quill("#editor", {
   },
   theme: "snow",
 });
+
+$('form[name="prdCreateForm"]').submit(onSubmitPrdCreateForm);
+function onSubmitPrdCreateForm(e) {
+  e.preventDefault();
+  var title = this.title.value.trim();
+  if (title === "") {
+    this.title.focus();
+    return false;
+  }
+  this.submit();
+}
